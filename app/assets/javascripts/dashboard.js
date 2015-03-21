0a1813e3767e433a5726ce1107d85a88d575571b
Oscar Blanco <oscarbc1996@gmail.com>
2015-03-21 16:55:05 +0100

    Added vertically and horitzontal center

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,15 +1,15 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).ready(function() {
-  $(".widget-container").on("click", function(){
-    $(this).addClass( "widget-active" );
-    $(this).parent().removeClass( "col-md-4" );
-  });
+// $(document).ready(function() {
+//   $(".widget-container").on("click", function(){
+//     $(this).addClass( "widget-active" );
+//     $(this).parent().removeClass( "col-md-4" );
+//   });
 
-  $(".close").on( "click", function(e){
-    $(this).parent().removeClass( "widget-active" );
-    $(this).parent().parent().addClass( "col-md-4" );
-     e.stopPropagation();
-  });
-});
+//   $(".close").on( "click", function(e){
+//     $(this).parent().removeClass( "widget-active" );
+//     $(this).parent().parent().addClass( "col-md-4" );
+//      e.stopPropagation();
+//   });
+// });
