8339f73de4531f006c2065e051a4db46121d2aa3
Oscar Blanco <oscarbc1996@gmail.com>
2015-03-21 18:59:25 +0100

    added grid layout

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -13,3 +13,9 @@
 //      e.stopPropagation();
 //   });
 // });
+$(document).ready(function() {
+  $(".gridster ul").gridster({
+    widget_margins: [10, 10],
+    widget_base_dimensions: [140, 140]
+  });
+});
