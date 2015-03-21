d783181ecaead2f7eafbd52929d38ed63236b6c1
EduRivera <eduardriveramestre@gmail.com>
2015-03-21 19:37:50 +0100

    add slide var

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -16,6 +16,7 @@
 $(document).ready(function() {
   $(".gridster ul").gridster({
     widget_margins: [10, 10],
-    widget_base_dimensions: [140, 140]
+    widget_base_dimensions: [140, 140], 
+    max_size_x: 9
   });
 });
