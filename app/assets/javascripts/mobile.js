38d6881866728d18b79b1f8beb09b3d4fe50265b
Oscar Blanco <oscarbc1996@gmail.com>
2015-03-22 03:37:24 +0100

    added js for tab bar

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
 
-  $(".container div").hide();
+  $(".innerContainer").hide();
   $(".showing").show();
   $('.tabbar a').click(function (e) {
     e.preventDefault();
-    // if ($(this).data("container") == ) {};
-    $(".showing").hide();
-    $(".showing").removeClass("showing");
-    var container = $(this).data("container");
-    $(container).show();
+    if ($(this).data("container") != $(".showing").data("container") {
+      $(this).parent().removeClass("active");
+      $(".showing").hide();
+      $(".showing").removeClass("showing");
+      var container = $(this).data("container");
+      $(container).show();
+      $(this).parent().addClass("active");
+    };
   });
 });
